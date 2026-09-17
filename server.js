86a1f450de27fdb5ee08a3980253b5a493ab2204
Furyyking <halilcan.sarbuga@gmail.com>
2026-09-18 01:51:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,8 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+socket.on("chat message", (data) => {
+  if (!data || !data.text) return;
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(__dirname));
-
-io.on("connection", (socket) => {
-  console.log("Bir kullanıcı bağlandı");
-
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  io.emit("chat message", {
+    nick: data.nick || users.get(socket.id) || "Misafir",
+    text: data.text
   });
-
-  socket.on("disconnect", () => {
-    console.log("Bir kullanıcı ayrıldı");
-  });
-});
-
-server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Sunucu ${PORT} portunda çalışıyor`);
 });
